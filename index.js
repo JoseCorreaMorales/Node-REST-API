9b81ddbefa8cc54b5c431324f1a9bb57dc0c9485
JoseCorreaMorales <jose.correax@gmail.com>
2023-09-04 18:59:23 -0600

    products router

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const debug = require('debug')('app:main');
 
 const { Config } = require('./src/config');
+const { ProductsAPI } = require('./src/products');
 
 const app = express();
 
 app.use(express.json());
 
 
+ProductsAPI(app);
 
 app.listen(Config.port, () => {
     debug(`Server is running on port ${Config.port}`)
